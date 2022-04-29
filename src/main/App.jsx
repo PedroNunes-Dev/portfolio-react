38d1d20ee09fb8bebe62a908681242e4620d4ec2
Pedro Nunes <adm.pedronunes@hotmail.com>
2022-04-28 23:55:36 -0300

    clearnig codes

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -2,11 +2,6 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from "react-router-dom";
 
-import Home from "../components/Home/Home"
-import GitUser from "../components/GitUser/Git";
-import Projects from "../components/Projects/Projects";
-import Tecnologies from "../components/Tecnologies/Tecnologies";
-import About from "../components/About/About";
 import Menu from "../components/Header/Header";
 import Routes from "./Routes";
 
